665b689431d1e116f9644eb92febb1e92492c6a8
unknown <helfese@gmail.com>
2024-12-09 23:30:40 +0000

    Add histogram and change to deepchem

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-url = "https://raw.githubusercontent.com/dataprofessor/data/master/delaney.csv"
+import deepchem as dc
 
 from rdkit import Chem
 from rdkit.Chem import Descriptors as dp
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(url)
+_, (df, _, _), _ = dc.molnet.load_delaney(featurizer='ECFP')
+df = pd.DataFrame(data={'SMILES': df.ids, 'LogS': df.y.flatten()})
+
 mols = [Chem.MolFromSmiles(mol) for mol in df.SMILES]
 
 def genDescLogS(smiles):
@@ -26,16 +28,21 @@ df2 = pd.DataFrame(aromaticAtoms(df.SMILES), columns=['Aromatic Proportion'])
 x = pd.concat([df1, df2], axis=1)
 y = df.iloc[:, 1]
 
+plt.figure(figsize=(4.25, 8.5))
+plt.subplot(2, 1, 1)
+y.hist(grid=0, color='gray')
+
 x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 model = linear_model.LinearRegression()
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
+plt.subplot(2, 1, 2)
 plt.scatter(y_test, y_pred, c='gray', alpha=0.25)
 p = np.poly1d(np.polyfit(y_test, y_pred, 1))
 plt.plot(y_test, p(y_test), color='black', alpha=0.35)
 
-plt.title('Aqueous Solubility')
+plt.title('Molecular Solubility')
 plt.ylabel('Predicted')
 plt.xlabel('Experimental')
